f4857e36cac6285e8cea16e37b6b7e73993bb072
Lloyd Lobo <lloydlobo4@gmail.com>
2023-12-01 10:48:47 +0530

    feat: use smart pointers to manage memory

--- main.cpp
+++ main.cpp
@@ -4,55 +4,59 @@
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <memory>
 #include <vector>
 
 struct HashTable {
-    HashTable(size_t size) : m_size(size), m_table(m_size, nullptr) {}
+    HashTable(size_t size) : m_size(size), m_table(m_size) {}
 
     ~HashTable() {
-        for (Entry* entry : m_table) {
+        for (auto &entry : m_table) {
             while (entry != nullptr) {
-                Entry* tmp = entry;
-                entry = entry->next;
-                delete tmp;
+                auto tmp = std::move(entry);
+                entry = std::move(tmp->next);
             }
         }
     }
 
-    void insert(const char* key, const int val) {
+    void insert(const char *key, const int val) {
         uint64_t index = djb2_hash(key);
-        Entry* entry = new Entry{key, val};
+        auto entry = std::make_unique<Entry>(key, val);
 
+        // Key not found, insert new entry
         if (m_table[index] == nullptr) {
-            m_table[index] = entry;
-        } else {
-            Entry* cur = m_table[index];
+            m_table[index] = std::move(entry);
+            return;
+        }
 
-            while (cur != nullptr) {
-                if (cur->key == key) {
-                    cur->val = val;
-                }
-                cur = cur->next;
+        auto cur = m_table[index].get();
+
+        while (cur != nullptr) {
+            if (std::strcmp(cur->key, key) == 0) {
+                cur->val = val;
+                return;
             }
+            if (cur->next == nullptr) break;
+
+            cur = cur->next.get();
         }
     }
 
-    int get(const char* key) {
-        uint64_t index = djb2_hash(key);
-        Entry* cur = m_table[index];
-        while (cur != nullptr) {
-            if (cur->key == key) {
-                return cur->val;
-            }
-            cur = cur->next;
+    int get(const char *key) {
+        auto cur_entry = m_table[djb2_hash(key)].get();
+
+        while (cur_entry != nullptr) {
+            if (std::strcmp(cur_entry->key, key) == 0) return cur_entry->val;
+            cur_entry = cur_entry->next.get();
         }
         return -1;  // Key not found
     }
 
+   private:
     // `djb2` Bernstein hash function updates the hash value using the formula
     // `(hash << 5) + hash + *key`. It iterates through each character,
     // left-shifting the current hash by 5 bits and adding the ASCII value.
-    uint64_t djb2_hash(const char* key) {
+    uint64_t djb2_hash(const char *key) {
         uint64_t hash = 5381;
         while (*key != '\0') {
             hash = (((hash << 5) + hash) + *key);  // hash * 33 + c;
@@ -61,30 +65,33 @@ struct HashTable {
         return (hash % m_size);
     }
 
-   private:
     struct Entry {
-        const char* key;
+        const char *key;
         int val;
-        Entry* next;
-    };
+        std::unique_ptr<Entry> next;
 
+        Entry(const char *k, int v) : key(k), val(v), next(nullptr) {}
+    };
     const size_t m_size;
-    std::vector<Entry*> m_table;
+    std::vector<std::unique_ptr<Entry>> m_table;
 };
 
 int main() {
-    HashTable ht(100);  // Initialize the hash table
+    const char *const keys[3] = {"puppy", "kitty", "horsie"};
+    const int values[3] = {5, 8, 12};
+
+    // Initialize the hash table
+    HashTable ht(100);
 
     // Insert some key-value pairs
-    ht.insert("puppy", 5);
-    ht.insert("kitty", 8);
-    ht.insert("birdy", 12);
-    ht.insert("puppy", 7);
+    for (size_t i = 0; i < 3; i += 1) ht.insert(keys[i], values[i]);
+
+    ht.insert("puppy", 7);  // Update a key
 
     // Retrieve values
     std::cout << "Count of puppy': " << ht.get("puppy") << std::endl;
     std::cout << "Count of kitty': " << ht.get("kitty") << std::endl;
-    std::cout << "Count of wolfie': " << ht.get("")
+    std::cout << "Count of wolfie': " << ht.get("wolfie")
               << std::endl;  // Should print -1
 
     return 0;
